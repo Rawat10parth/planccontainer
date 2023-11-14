c03e61176887fcad5d0bfc47512f64f4905bf8a8
Parth Rawat <parthpriyanshu10@gmail.com>
2023-11-14 14:37:09 +0530

    added files

---
+++ c-env/user-service/user_service.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import flask_sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    with app.app_context():
+        user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password_hash, password):
+        return jsonify({'message': 'Login successful'})
+    else:
+        return jsonify({'message': 'Login failed'})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=5001)
